dba99a8ead12299da0b97cdf4dedb28388550515
polin103109 <sabrinpolinece18@gmail.com>
2023-07-07 15:35:20 +0600

    added validation part to form1

--- src/components/Steponeform.jsx
+++ src/components/Steponeform.jsx
@@ -5,8 +5,42 @@ const FormStep1 = ( {onNext}) => {
     const [projectDescription, setProjectDescription] = useState('');
     const [client, setClient] = useState('');
     const [contractor, setContractor] = useState('');
+    const [formErrors, setFormErrors] = useState({
+        projectName: '',
+        projectDescription: '',
+        client: '',
+        contractor: '',
+      });
     const handleNext = (event) => {
         event.preventDefault();
+        if (!projectName.trim()) {
+            setFormErrors(prevErrors => ({
+              ...prevErrors,
+              projectName: 'Project Name is required',
+            }));
+            return;
+          }
+          if (!projectDescription.trim()) {
+            setFormErrors(prevErrors => ({
+              ...prevErrors,
+              projectDescription: 'Project Description is required',
+            }));
+            return;
+          }
+          if (!client.trim()) {
+            setFormErrors(prevErrors => ({
+              ...prevErrors,
+              client: 'Client is required',
+            }));
+            return;
+          }
+          if (!contractor.trim()) {
+            setFormErrors(prevErrors => ({
+              ...prevErrors,
+              contractor: 'Contractor is required',
+            }));
+            return;
+          }
         onNext({
           projectName,
           projectDescription,
@@ -22,12 +56,16 @@ const FormStep1 = ( {onNext}) => {
         <h2 className="h2">Step 1</h2>
         <label>Project Name:</label>
         <input type="text" value={projectName} onChange={(e) => setProjectName(e.target.value)}  required/>
+        {formErrors.projectName && <p className="error">{formErrors.projectName}</p>}
         <label>Project Description:</label>
         <input type="text" value={projectDescription} onChange={(e) => setProjectDescription(e.target.value)}  required />
+        {formErrors.projectDescription && <p className="error">{formErrors.projectDescription}</p>}
         <label>Client:</label>
         <input type="text" value={client} onChange={(e) => setClient(e.target.value)}  required/>
+        {formErrors.client && <p className="error">{formErrors.client}</p>}
         <label>Contractor:</label>
         <input type="text" value={contractor} onChange={(e) => setContractor(e.target.value)}  required />
+        {formErrors.contractor && <p className="error">{formErrors.contractor}</p>}
         <button onClick={handleNext}>Next</button>
       </div>
     );
