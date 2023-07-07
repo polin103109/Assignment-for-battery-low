c9dfa93d017b342cd4634d2a78fe2da7946eff2e
polin103109 <sabrinpolinece18@gmail.com>
2023-07-07 19:57:31 +0600

    added and modified the validation

--- src/components/Steponeform.jsx
+++ src/components/Steponeform.jsx
@@ -13,59 +13,55 @@ const FormStep1 = ( {onNext}) => {
       });
     const handleNext = (event) => {
         event.preventDefault();
-        if (!projectName.trim()) {
-            setFormErrors(prevErrors => ({
-              ...prevErrors,
-              projectName: 'Project Name is required',
-            }));
-            return;
-          }
-          if (!projectDescription.trim()) {
-            setFormErrors(prevErrors => ({
-              ...prevErrors,
-              projectDescription: 'Project Description is required',
-            }));
-            return;
-          }
-          if (!client.trim()) {
-            setFormErrors(prevErrors => ({
-              ...prevErrors,
-              client: 'Client is required',
-            }));
-            return;
-          }
-          if (!contractor.trim()) {
-            setFormErrors(prevErrors => ({
-              ...prevErrors,
-              contractor: 'Contractor is required',
-            }));
-            return;
-          }
-        onNext({
-          projectName,
-          projectDescription,
-          client,
-          contractor,
-        });
-      };
-  
-    
-  
-    return (
+        const newFormErrors = {};
+
+    if (!projectName.trim()) {
+      newFormErrors.projectName = "Project Name is required";
+    }
+    if (!projectDescription.trim()) {
+      newFormErrors.projectDescription = "Project Description is required";
+    }
+    if (!client.trim()) {
+      newFormErrors.client = "Client is required";
+    }
+    if (!contractor.trim()) {
+      newFormErrors.contractor = "Contractor is required";
+    }
+
+    if (Object.keys(newFormErrors).length === 0) {
+      onNext({
+        projectName,
+        projectDescription,
+        client,
+        contractor,
+      });
+    } else {
+      setFormErrors(newFormErrors);
+    }
+  };
+        
+return (
       <div className="container">
         <h2 className="h2">Step 1</h2>
         <label>Project Name:</label>
         <input type="text" value={projectName} onChange={(e) => setProjectName(e.target.value)}  required/>
-        {formErrors.projectName && <p className="error">{formErrors.projectName}</p>}
+        {formErrors.projectName && (
+        <p className="error">{formErrors.projectName}</p>
+      )}
+      
         <label>Project Description:</label>
         <input type="text" value={projectDescription} onChange={(e) => setProjectDescription(e.target.value)}  required />
-        {formErrors.projectDescription && <p className="error">{formErrors.projectDescription}</p>}
+        {formErrors.projectDescription && (
+        <p className="error">{formErrors.projectDescription}</p>
+      )}
         <label>Client:</label>
         <input type="text" value={client} onChange={(e) => setClient(e.target.value)}  required/>
         {formErrors.client && <p className="error">{formErrors.client}</p>}
         <label>Contractor:</label>
         <input type="text" value={contractor} onChange={(e) => setContractor(e.target.value)}  required />
-        {formErrors.contractor && <p className="error">{formErrors.contractor}</p>}
+        {formErrors.contractor && (
+        <p className="error">{formErrors.contractor}</p>
+      )}
         <button onClick={handleNext}>Next</button>
       </div>
     );
